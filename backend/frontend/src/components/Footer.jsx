ac07827f303220dd05db6e2b2c1fc8a9a239a8bb
navta-edu <navta.edu@gmail.com>
2026-08-14 15:02:24 +0530

    Update Footer.jsx

--- backend/frontend/src/components/Footer.jsx
+++ backend/frontend/src/components/Footer.jsx
@@ -5,6 +5,7 @@
   </div>
   <!-- Right Side: Developer Info -->
   <div class="footer-right">
+    
     Designed & Developed by 
     <a href="www.linkedin.com/in/hardik-sahu-9797h1608p" target="_blank" rel="noopener noreferrer">Hardik Sahu</a>
   </div>
